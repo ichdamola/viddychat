5e71eace28a20fc782078b7d0448024924479aaf
Adedapo Adedamola <adedadamola@gmail.com>
2021-06-10 14:42:38 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const app = require("express")();
 const server = require("http").createServer(app);
-const cors = require(cors);
+const cors = require("cors");
 
-const io = require(socket.io)(server, {
+const io = require("socket.io")(server,{
         cors: {
-            origin: "*";
-            motheds: ["GET", "POST"];
+            origin: "*",
+            motheds: ["GET", "POST"],
         }
     });
 
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/', function (req, res) {
+    res.send('Server is running...');
+  });
+
+  server.listen(PORT, function (){
+      console.log(`Server listening on PORT ${PORT}`);
+  })
