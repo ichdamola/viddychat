65bc9c2a103069c4e3675c21811e7b29326180a9
Adedapo Adedamola <adedadamola@gmail.com>
2021-06-13 19:27:09 +0100

    Render the context provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import App from './App';
+import { ContextProvider }  from './SocketContext';
 import './style.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+    document.getElementById('root')
+);
